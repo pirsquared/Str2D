e1cab65be6880979a83077612ba0a5eddd803d6e
ReblochonMasque <trustgold@gmail.com>
2018-01-04 14:39:53 +0800

    add tests for Perforation enum add imports to init

---
+++ stencil/__init__.py
@@ -0,0 +1,4 @@
+from stencil.stencil import Mask
+from stencil.stencil import Perforation
+from stencil.stencil import SOLID, PUNCHED
+from stencil.stencil import Stencil
--- tests_stencils/__init__.py
+++
@@ -1,1 +0,0 @@
-from mask.stencil import Mask
