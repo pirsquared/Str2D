87fab15fdf31aff67a5c6d98e5a57cac2a8afa6a
ReblochonMasque <trustgold@gmail.com>
2018-01-04 14:44:18 +0800

    add note for future imports to add to maintain a flat structure

--- str2d/__init__.py
+++ str2d/__init__.py
@@ -1,2 +1,9 @@
 from str2d.str2d import Str2D
 from str2d.str2d import utils
+
+# will need (I think) to add the following, once mask/stencil is extracted,
+# in order to maintain the appearance of a  flat import structure for clients
+# from stencil.stencil import Mask
+# from stencil.stencil import Perforation        #<-- maybe not needed
+# from stencil.stencil import SOLID, PUNCHED     #<-- maybe not needed
+# from stencil.stencil import Stencil
