30b1ca648d248dd43d13606936b11ca3fe3a8786
ReblochonMasque <trustgold@gmail.com>
2018-01-04 16:36:53 +0800

    add precisions and enlarged the scope of existing tests

--- tests_stencils/test_mask.py
+++ tests_stencils/test_mask.py
@@ -11,17 +11,21 @@ class TestMask(unittest.TestCase):
         self.blank_ready_to_punch = Mask(size=12)
 
         self.all_punched_mask = Mask(size=12)
-        # protected access to make a 'mock'
+        # protected access to inject values and make an instance outside of factory
         self.all_punched_mask._mask = tuple(PUNCHED for _ in range(12))
 
     def test_blank_mask_instance(self):
         self.assertIsInstance(self.blank_that_remains_blank, Mask)
 
     def test_blank_is_a_blank(self):
+        """test is_a_blank with a blank"""
         self.assertTrue(self.blank_that_remains_blank.is_a_blank())
+        self.assertTrue(all(pos is SOLID for pos in self.blank_that_remains_blank.mask))
 
     def test_mask_is_not_a_blank(self):
+        """test is_a_blank with a mask whose values were injected"""
         self.assertFalse(self.all_punched_mask.is_a_blank())
+        self.assertTrue(any(pos is not SOLID for pos in self.all_punched_mask.mask))
 
     def test_all_punched_mask_mask(self):
         pass
