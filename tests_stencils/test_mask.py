4397e524e0b8131bea92366b2a33693f04c88927
ReblochonMasque <trustgold@gmail.com>
2018-01-05 12:50:22 +0800

    add tests for invert_mask() in new test class for mask usage

--- tests_stencils/test_mask.py
+++ tests_stencils/test_mask.py
@@ -22,13 +22,6 @@ class TestMaskInternals(unittest.TestCase):
         self.all_punched_mask = Mask(size=12)
         self.all_punched_mask._mask = tuple(PUNCHED for _ in range(12))
 
-        # the two following instances are 'negative' of each other and
-        # can be compared via the invert() method
-        self.odd_are_punched = Mask(size=12)
-        self.odd_are_punched._mask = tuple(SOLID if pos % 2 else PUNCHED for pos in range(12))
-        self.even_are_punched = Mask(size=12)
-        self.even_are_punched._mask = tuple(PUNCHED if pos % 2 else SOLID for pos in range(12))
-
     # -------------- TEST INSTANCE and INIT ---------------------------------
     def test_blank_mask_instance(self):
         self.assertIsInstance(self.blank_that_remains_blank, Mask)
@@ -105,6 +98,61 @@ class TestMaskInternals(unittest.TestCase):
         self.blank_ready_to_punch._punch_mask([True for _ in range(self.blank_ready_to_punch.size)])
         self.assertEqual(self.all_punched_mask, self.blank_ready_to_punch)
     # -------------- END TEST _punch_mask ---------------------------------------------
+
+
+class TestMaskUsage(unittest.TestCase):
+    """tests for the internals of Mask"""
+
+    def setUp(self):
+        self.blank_that_remains_blank = Mask(size=12)
+        self.blank_ready_to_punch = Mask(size=12)
+
+        self.mask1__0_1_0 = Mask(size=3)
+        self.mask1__0_1_0._punch_mask([False, True, False])
+        self.mask2__0_1_0 = Mask(size=3)
+        self.mask2__0_1_0._punch_mask([False, True, False])
+
+        # all instances of Mask created below, but within setUp() require
+        # protected access to inject values and avoid using the methods
+        # of Mask or the Mask factories to build
+        self.all_punched_mask = Mask(size=12)
+        self.all_punched_mask._mask = tuple(PUNCHED for _ in range(12))
+
+        # the two following instances are 'negative' of each other and
+        # can be compared via the invert() method
+        self.odd_are_punched = Mask(size=12)
+        self.odd_are_punched._mask = tuple(SOLID if pos % 2 else PUNCHED for pos in range(12))
+        self.even_are_punched = Mask(size=12)
+        self.even_are_punched._mask = tuple(PUNCHED if pos % 2 else SOLID for pos in range(12))
+
+    # -------------- TEST invert_mask -------------------------------------------------
+    def test_invert_does_not_mutate_original_mask(self):
+        self.assertEqual(self.mask2__0_1_0, self.mask1__0_1_0)  # two masks equal before
+        self.mask1__0_1_0.invert_mask()
+        self.assertEqual(self.mask2__0_1_0, self.mask1__0_1_0)  # still equal after
+
+    def test_invert_full_solid(self):
+        # this is also accessing the internals...
+        fully_punched = self.blank_ready_to_punch.invert_mask()
+        self.assertTrue(fully_punched._mask == tuple(PUNCHED for _ in range(self.blank_ready_to_punch.size)))
+
+    def test_invert_full_punched(self):
+        full_solid = self.all_punched_mask.invert_mask()
+        self.assertTrue(full_solid.is_a_blank())
+
+    def test_0101___to_1010___(self):
+        self.assertEqual(self.odd_are_punched.invert_mask(), self.even_are_punched)
+
+    def test_1010___to_0101___(self):
+        self.assertEqual(self.even_are_punched.invert_mask(), self.odd_are_punched)
+
+    def test_invert_and_then_revert_to_original(self):
+        go = self.even_are_punched.invert_mask()
+        back = go.invert_mask()
+        self.assertEqual(back, self.even_are_punched)
+    # -------------- END TEST invert_mask ---------------------------------------------
+
+
 
 
 
