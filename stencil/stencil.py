bc3af4a0a02aed72e3fa41df088b41c26c1d1afe
ReblochonMasque <trustgold@gmail.com>
2018-01-09 15:49:17 +0800

    clarify stencil docstring add tentative __init__ to stencil

--- stencil/stencil.py
+++ stencil/stencil.py
@@ -1,24 +1,14 @@
 
 
-class Crib:
-    """a superposition of several `Stencil`, where the perforated positions
-    remain perforated if all `Stencil` are perforated at that position, and
-    blocked otherwise
-    (A la "breaking the Enigma" kind of Crib)
-
-    # this could maybe be a function or method `make_crib(list of Stencil)`,
-    # returning an aggregate Stencil?
-
-    # Note (that probably takes us far out of scope):
-    # Stencils forming a Crib could be moved/perturbated, to discover a
-    # pattern?
-    # the Masks forming a Stencil could also be slided?
-    """
-
-
 class Stencil:
-    """a "cover" on a grid surface that is "perforated" at some positions to
-    let the elements through, while the other positions are blocked and
-    conceal the elements.
-    an aggregate of `Mask`
+    """a rectangular "cover" on a grid surface that is "perforated" at some
+    positions to let the elements through, while the other positions are
+    blocked and conceal the elements.
+    an aggregate of `Mask` of same length, defining a rectangle of
+    SOLID and PUNCHED positions
     """
+    def __init__(self):
+        self.num_rows: int = 0
+        self.num_cols: int = 0
+        self.masks = None        # array of blanks [Mask(size=num_cols) for _ in range(num_rows)]
+        self.patterns = None
